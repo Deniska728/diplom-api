f26a75f4e970a4b7dab2764373ed7ccd9ac52f27
Deniska728 <denis790504790@gmail.com>
2020-05-22 17:49:27 +0300

    Update create-schema mutation

--- src/api/schemas/mutations/create-schema.js
+++ src/api/schemas/mutations/create-schema.js
@@ -3,17 +3,17 @@ import faker from 'faker';
 
 import getIntrospectionResult from '../../schema-versions/functions/get-introspection-result';
 
-export default async (root, { name, endpoint }, { db, user }) => {
+export default async (root, { name, endpoint }, { prisma, user }) => {
   if (!user) throw new Error('Access denied');
 
-  const schemasCount = await db.gqlSchemasConnection({ where: { owner: { id: user.id } } })
+  const schemasCount = await prisma.gqlSchemasConnection({ where: { owner: { id: user.id } } })
     .aggregate()
     .count();
   if (schemasCount > 10) throw new Error('No more than 10 schemas can be created');
 
   const introspectionQuery = await getIntrospectionResult(endpoint);
 
-  const schema = await db.createGqlSchema({
+  const schema = await prisma.createGqlSchema({
     name: name || faker.commerce.productName(),
     apiKey: crypto.randomBytes(24).toString('hex'),
     owner: { connect: { id: user.id } },
---
+++ src/api/schemas/mutations/index.js
@@ -0,0 +1,5 @@
+import createSchema from './create-schema';
+
+export default {
+  createSchema,
+};
