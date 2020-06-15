79587434b50ac51dbe86d8f6de8cfc1f4145b785
Denis Rabov <denis@milkandcartoons.com>
2020-06-16 02:55:33 +0300

    Update nodemailer config

--- src/integrations/emails/nodemailer.js
+++ src/integrations/emails/nodemailer.js
@@ -3,7 +3,9 @@ import nodemailer from 'nodemailer';
 const { MAIL_URL, MAIL_PASS } = process.env;
 
 export default nodemailer.createTransport({
-  service: 'gmail',
+  port: 465,
+  host: 'smtp.gmail.com',
+  sequre: true,
   auth: {
     user: MAIL_URL,
     pass: MAIL_PASS,
